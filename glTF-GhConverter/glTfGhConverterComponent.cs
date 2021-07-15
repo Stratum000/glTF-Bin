1f15ab148e79134f96c175fb79aedc04553a7532
Bill gs Apps <gsapps@live.com>
2021-07-15 11:59:54 -0700

    Ready to use

--- glTF-GhConverter/glTfGhConverterComponent.cs
+++ glTF-GhConverter/glTfGhConverterComponent.cs
@@ -1,5 +1,8 @@
+using glTF_BinExporter;
 using Grasshopper;
 using Grasshopper.Kernel;
+using Rhino;
+using Rhino.DocObjects;
 using Rhino.Geometry;
 using System;
 using System.Collections.Generic;
@@ -9,99 +12,70 @@ namespace GhGltfConverter
     public class glTfGhConverterComponent : GH_Component
     {
         /// <summary>
-        /// Each implementation of GH_Component must provide a public 
-        /// constructor without any arguments.
-        /// Category represents the Tab in which the component will appear, 
-        /// Subcategory the panel. If you use non-existing tab or panel names, 
-        /// new tabs/panels will automatically be created.
+        /// Initialize the Component display info
         /// </summary>
         public glTfGhConverterComponent()
-          : base("glTF-GhConverter", "glTF",
-            "Convert mesh to glTF",
-            "Stratum", "")
+          : base("glTF-GhConverter", "glTF", "Convert mesh to glTF", "Stratum", "")
         {
         }
 
         /// <summary>
-        /// Registers all the input parameters for this component.
+        /// Register all the input parameters for this component.
         /// </summary>
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
-            // Use the pManager object to register your input parameters.
-            // You can often supply default values when creating parameters.
-            // All parameters must have the correct access type. If you want 
-            // to import lists or trees of values, modify the ParamAccess flag.
             pManager.AddMeshParameter("Mesh", "M", "Mesh to be converted", GH_ParamAccess.item);
-
-            // If you want to change properties of certain parameters, 
-            // you can use the pManager instance to access them by index:
-            //pManager[0].Optional = true;
+            pManager.AddBooleanParameter("Draco", "D", "Draco compression", GH_ParamAccess.item, true);
         }
 
         /// <summary>
-        /// Registers all the output parameters for this component.
+        /// Register all the output parameters for this component.
         /// </summary>
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
-            // Use the pManager object to register your output parameters.
-            // Output parameters do not have default values, but they too must have the correct access type.
             pManager.AddTextParameter("glTF", "T", "glTF", GH_ParamAccess.item);
-
-            // Sometimes you want to hide a specific parameter from the Rhino preview.
-            // You can use the HideParameter() method as a quick way:
-            //pManager.HideParameter(0);
         }
 
         /// <summary>
-        /// This is the method that actually does the work.
+        /// Do the work.
         /// </summary>
         /// <param name="DA">The DA object can be used to retrieve data from input parameters and 
         /// to store data in output parameters.</param>
         protected override void SolveInstance(IGH_DataAccess DA)
         {
-            // First, we need to retrieve all data from the input parameters.
-
+            // retrieve all data from the input parameters.
             Mesh mesh = null;
-            TextEntity glTFtext = null;
+            bool doDraco = true;
+            string glTFtext;
 
             // When data cannot be extracted from a parameter, we should abort this method.
             if (!DA.GetData(0, ref mesh)) return;
+            if (!DA.GetData(1, ref doDraco)) return;
 
-            // We should now validate the data and warn the user if invalid data is supplied.
-            //if (radius0 < 0.0)
-            //{
-            //    AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Inner radius must be bigger than or equal to zero");
+            // pattern for input validation, if required
+            //    AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "error message here");
             //    return;
             //}
 
+            // the converter wants a Rhino doc
+            RhinoDoc rhinoDoc = RhinoDoc.Create(null);
+            rhinoDoc.Objects.AddMesh(mesh);
+            
+            glTFExportOptions opts = new glTFExportOptions();
+            opts.UseDracoCompression = doDraco;
 
-            // Finally assign the output parameter.
-            DA.SetData(0, "foo");
+            glTFtext = DoConversion(opts, rhinoDoc.Objects, rhinoDoc.RenderSettings.LinearWorkflow);
+
+            // assign the output parameter.
+            DA.SetData(0, glTFtext);
+
         }
 
-        Curve CreateSpiral(Plane plane, double r0, double r1, Int32 turns)
+        public string DoConversion(glTFExportOptions options, IEnumerable<RhinoObject> rhinoObjects, Rhino.Render.LinearWorkflow workflow)
         {
-            Line l0 = new Line(plane.Origin + r0 * plane.XAxis, plane.Origin + r1 * plane.XAxis);
-            Line l1 = new Line(plane.Origin - r0 * plane.XAxis, plane.Origin - r1 * plane.XAxis);
-
-            Point3d[] p0;
-            Point3d[] p1;
-
-            l0.ToNurbsCurve().DivideByCount(turns, true, out p0);
-            l1.ToNurbsCurve().DivideByCount(turns, true, out p1);
-
-            PolyCurve spiral = new PolyCurve();
-
-            for (int i = 0; i < p0.Length - 1; i++)
-            {
-                Arc arc0 = new Arc(p0[i], plane.YAxis, p1[i + 1]);
-                Arc arc1 = new Arc(p1[i + 1], -plane.YAxis, p0[i + 1]);
-
-                spiral.Append(arc0);
-                spiral.Append(arc1);
-            }
-
-            return spiral;
+            RhinoDocGltfConverter converter = new RhinoDocGltfConverter(options, false, rhinoObjects, workflow);
+            glTFLoader.Schema.Gltf gltf = converter.ConvertToGltf();
+            return glTFLoader.Interface.SerializeModel(gltf);
         }
 
         /// <summary>
