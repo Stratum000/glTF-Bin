df4e92deeba9e019c740573f1ea111c5c7d0e2ae
Bill gs Apps <gsapps@live.com>
2021-07-21 17:30:31 -0700

    Added simple materials as inputs to the converter

--- glTF-GhConverter/glTfGhConverterComponent.cs
+++ glTF-GhConverter/glTfGhConverterComponent.cs
@@ -1,11 +1,15 @@
 using glTF_BinExporter;
 using Grasshopper;
 using Grasshopper.Kernel;
+using Grasshopper.Kernel.Data;
+using Grasshopper.Kernel.Types;
+using Newtonsoft.Json;
 using Rhino;
 using Rhino.DocObjects;
 using Rhino.Geometry;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace GhGltfConverter
 {
@@ -24,8 +28,10 @@ namespace GhGltfConverter
         /// </summary>
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
-            pManager.AddMeshParameter("Meshes", "M", "Meshes to be converted", GH_ParamAccess.list);
-            pManager.AddBooleanParameter("Draco", "D", "Draco compression", GH_ParamAccess.item, false);
+            pManager.AddMeshParameter("Meshes", "Mesh", "Meshes to be converted", GH_ParamAccess.list);
+            pManager.AddIntegerParameter("Material indices", "Idx", "Material index for each mesh", GH_ParamAccess.list, 0);
+            pManager.AddTextParameter("Materials", "Mat", "Materials JSON", GH_ParamAccess.list);
+            pManager.AddBooleanParameter("Draco", "Draco", "Draco compression", GH_ParamAccess.item, false);
         }
 
         /// <summary>
@@ -45,8 +51,9 @@ namespace GhGltfConverter
         {
             // retrieve all data from the input parameters.
             List<Mesh> meshes = new List<Mesh>();
+            List<int> materialIndices = new List<int>();  // a list, same size as meshes, with the material index for the corresponding mesh
+            List<string> materialSpecs = new List<string>();
             bool doDraco = true;
-            string glTFtext;
 
             // When data cannot be extracted from a parameter, abort.
             if (!DA.GetDataList(0, meshes))
@@ -54,42 +61,133 @@ namespace GhGltfConverter
                 AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Unable to get Meshes input");
                 return;
             }
-            if (!DA.GetData(1, ref doDraco))
+            if (!DA.GetDataList(1, materialIndices))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Unable to get material indices");
+                return;
+            }
+            if (!DA.GetDataList(2, materialSpecs))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Unable to get material specs");
+                return;
+            }
+            if (!DA.GetData(3, ref doDraco))
             {
                 AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Unable to get Draco input");
                 return;
             }
 
             // the converter wants a Rhino doc
-            RhinoDoc rhinoDoc = RhinoDoc.Create(null);
+            RhinoDoc rhinoDoc = RhinoDoc.CreateHeadless(null);
             meshes.ForEach(m => rhinoDoc.Objects.AddMesh(m));
 
             glTFExportOptions opts = new glTFExportOptions();
             opts.UseDracoCompression = doDraco;
             opts.UseDisplayColorForUnsetMaterials = true;  // if "false" still works here, but fails in Rhino server
 
-            glTFtext = DoConversion(opts, rhinoDoc.Objects, rhinoDoc.RenderSettings.LinearWorkflow);
-
+            glTFLoader.Schema.Gltf gltf = DoConversion(opts, rhinoDoc.Objects, rhinoDoc.RenderSettings.LinearWorkflow);
             rhinoDoc.Objects.Clear();
             rhinoDoc.Dispose();
 
+            SetMaterials(gltf, materialIndices, materialSpecs);
+
             // assign the output parameter.
-            DA.SetData(0, glTFtext);
+            DA.SetData(0, glTFLoader.Interface.SerializeModel(gltf));
         }
 
-        public string DoConversion(glTFExportOptions options, IEnumerable<RhinoObject> rhinoObjects, Rhino.Render.LinearWorkflow workflow)
+        private class InterimMaterialSpec
+        {
+            public float[] EmissiveFactor;
+            public float MetallicFactor;
+            public float RoughnessFactor;
+        };
+
+        private void SetMaterials(glTFLoader.Schema.Gltf gltf, List<int> materialIndices, List<string> materialSpecs)
+        {
+            // Had hoped to pass in a JSON string to serialize into MaterialSpec's, but JSON currently has a version conflict in Rhino. Maybe it goes away someday
+            // For now, the material specs are passed in as strings, each of which contains 5 numeric substrings.
+            // The correspond, in order, to 1,2,3) the RGB values for EmissiveFactor, 4) MetallicFactor, 5) RoughnessFactor.
+            // Thus the following super-kludge loop (this will be one line of code when JSON becomes available.)
+            List<InterimMaterialSpec> interimMaterials = new List<InterimMaterialSpec>();
+            foreach (string mString in materialSpecs)
+            {
+                List<string> specs = mString.Split(';').ToList();
+                List<float> floats = (from numStr in specs select (float)Convert.ToDouble(numStr)).ToList();
+                InterimMaterialSpec interimMaterial = new InterimMaterialSpec();
+                interimMaterial.EmissiveFactor = new float[] { floats[0], floats[1], floats[2], 1f };
+                interimMaterial.MetallicFactor = floats[3];
+                interimMaterial.RoughnessFactor = floats[4];
+                interimMaterials.Add(interimMaterial);
+            }
+
+            List<glTFLoader.Schema.Material> gltfMaterials = new List<glTFLoader.Schema.Material>();
+            foreach (InterimMaterialSpec im in interimMaterials)
+            {
+                glTFLoader.Schema.Material mat = new glTFLoader.Schema.Material();
+                //mat.EmissiveFactor = im.EmissiveFactor;
+                mat.PbrMetallicRoughness = new glTFLoader.Schema.MaterialPbrMetallicRoughness();
+                mat.PbrMetallicRoughness.MetallicFactor = im.MetallicFactor;
+                mat.PbrMetallicRoughness.RoughnessFactor = im.RoughnessFactor;
+                mat.PbrMetallicRoughness.BaseColorFactor = im.EmissiveFactor;
+
+                gltfMaterials.Add(mat);
+            }
+
+            int idx = 0;
+            int numMeshes = gltf.Meshes.Length;
+            int numFaces = numMeshes * 2 / 3;
+            foreach (glTFLoader.Schema.Mesh mesh in gltf.Meshes)
+            {
+                mesh.Primitives[0].Material = idx < numFaces ? 0 : 1;
+                idx++;
+            }
+
+            gltf.Materials = gltfMaterials.ToArray();
+
+
+        }
+
+        public glTFLoader.Schema.Gltf DoConversion(glTFExportOptions options, IEnumerable<RhinoObject> rhinoObjects, Rhino.Render.LinearWorkflow workflow)
         {
             RhinoDocGltfConverter converter = new RhinoDocGltfConverter(options, false, rhinoObjects, workflow);
-            glTFLoader.Schema.Gltf gltf = converter.ConvertToGltf();
+            return converter.ConvertToGltf();
 
             // temporary: modify the material of each object so it reflects a little
-            foreach (glTFLoader.Schema.Material m in gltf.Materials)
-            {
-                m.EmissiveFactor = new float[] { .62F, .32F, .18F };
-                m.PbrMetallicRoughness.MetallicFactor = .5F;
-                m.PbrMetallicRoughness.RoughnessFactor = .5F;
-            }
-            return glTFLoader.Interface.SerializeModel(gltf);
+            //foreach (glTFLoader.Schema.Material m in gltf.Materials)
+            //{
+            //    m.EmissiveFactor = new float[] { .62F, .32F, .18F };
+            //    m.PbrMetallicRoughness.MetallicFactor = .5F;
+            //    m.PbrMetallicRoughness.RoughnessFactor = .5F;
+            //}
+
+            //JsonConvert.DeserializeObject
+
+            //glTFLoader.Schema.Material mat1 = new glTFLoader.Schema.Material();
+            //mat1.EmissiveFactor = new float[] { .99F, .05F, .05F };
+            //mat1.PbrMetallicRoughness = new glTFLoader.Schema.MaterialPbrMetallicRoughness();
+            //mat1.PbrMetallicRoughness.MetallicFactor = .5F;
+            //mat1.PbrMetallicRoughness.RoughnessFactor = .5F;
+
+            //glTFLoader.Schema.Material mat2 = new glTFLoader.Schema.Material();
+            //mat2.EmissiveFactor = new float[] { .05F, .99F, .05F };
+            //mat2.PbrMetallicRoughness = new glTFLoader.Schema.MaterialPbrMetallicRoughness();
+            //mat2.PbrMetallicRoughness.MetallicFactor = .5F;
+            //mat2.PbrMetallicRoughness.RoughnessFactor = .5F;
+
+            //gltf.Materials = new glTFLoader.Schema.Material[] { mat1, mat2 };
+            //int idx = 0;
+            //int numMeshes = gltf.Meshes.Length;
+            //int numFaces = numMeshes * 2 / 3;
+            //foreach (glTFLoader.Schema.Mesh mesh in gltf.Meshes)
+            //{
+            //    mesh.Primitives[0].Material = idx < numFaces ? 0 : 1;
+            //    idx++;
+            //}
+
+            //return glTFLoader.Interface.SerializeModel(gltf);
+
+
+
         }
 
         /// <summary>
